0ff4d3557b0384bf7139ec49865b806e2b65b92b
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-07-30 17:53:39 -0600

    Update CosmosRailHook.php

--- includes/Hook/CosmosRailHook.php
+++ includes/Hook/CosmosRailHook.php
@@ -5,9 +5,11 @@ namespace MediaWiki\Skins\Cosmos\Hook;
 use Skin;
 
 interface CosmosRailHook {
+
 	/**
 	 * @param array &$modules
 	 * @param Skin $skin
+	 * @return void
 	 */
-	public function onCosmosRail( array &$modules, Skin $skin );
+	public function onCosmosRail( array &$modules, Skin $skin ): void;
 }
