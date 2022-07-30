13bdaacbd2072b93e08109028c51135821e0a5d1
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-07-30 17:22:14 -0600

    Update ServiceWiring.php

--- includes/ServiceWiring.php
+++ includes/ServiceWiring.php
@@ -3,12 +3,32 @@
 use MediaWiki\MediaWikiServices;
 use MediaWiki\Skins\Cosmos\CosmosBackgroundLookup;
 use MediaWiki\Skins\Cosmos\CosmosConfig;
+use MediaWiki\Skins\Cosmos\CosmosRail;
 use MediaWiki\Skins\Cosmos\CosmosWordmarkLookup;
+use MediaWiki\Skins\Hook\CosmosHookRunner;
 
 return [
 	'CosmosConfig' => static function ( MediaWikiServices $services ): CosmosConfig {
 		return new CosmosConfig(
 			$services->getConfigFactory()->makeConfig( 'Cosmos' )
+		);
+	},
+
+	'CosmosHookRunner' => static function ( MediaWikiServices $services ): CosmosHookRunner {
+		return new CosmosHookRunner(
+			$services->getHookContainer()
+		);
+	},
+
+	'CosmosRail' => static function ( MediaWikiServices $services ): CosmosRail {
+		return new CosmosRail(
+			$services->getService( 'CosmosConfig' ),
+			$services->getService( 'CosmosHookRunner' ),
+			$services->getDBLoadBalancer(),
+			$services->getLinkRenderer(),
+			$services->getMainWANObjectCache(),
+			$services->getSpecialPageFactory(),
+			$services->getUserFactory()
 		);
 	},
 
